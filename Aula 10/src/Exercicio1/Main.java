ed8f066eeb715c8d8d51e106abfe6f477c8afac8
LeticialMoraes <108829368+LeticialMoraes@users.noreply.github.com>
2023-05-10 08:13:31 -0300

    Update Main.java

--- Aula 10/src/Exercicio1/Main.java
+++ Aula 10/src/Exercicio1/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Calculadora c = new Calculadora(10, 0);
 
+        c.soma();
+        c.subtracao();
         try{
-            c.soma();
-            c.subtracao();
             c.multiplicacao();
             c.divisao();
         }catch (ArithmeticException e){
